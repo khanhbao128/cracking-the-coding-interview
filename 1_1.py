4f32a280c392f0b7897ad04e16259ef0e054023f
Khanh Duong <khanhbao128@yahoo.com>
2019-09-27 17:08:20 -0700

    contains_duplicates added

---
+++ 1_1.py
@@ -0,0 +1,16 @@
+# 1.1 Implement an algorithm to determine if a string has all unique characters. 
+# What if you cannot use additional data structures?
+
+def contains_duplicates(given_str):
+
+    letters = {}
+    for char in given_str:
+        if char in letters:
+            return False
+        else: 
+            letters[char] = True
+    return True
+
+
+print(contains_duplicates("hello"))
+print(contains_duplicates("ndhsko"))
